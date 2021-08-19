d294f30e35cb89194edea18a0bb27da9d8f18033
sunhuiquan <sunhuiquan@yeah.net>
2021-08-19 17:31:23 +0800

    add

--- car-rental-server/src/CarRentalRegister.cs
+++ car-rental-server/src/CarRentalRegister.cs
@@ -22,8 +22,9 @@ namespace car_rental_server
 
 				if (!Directory.Exists("/tmp/server_pic"))
 					Directory.CreateDirectory("/tmp/server_pic");
-				if (!File.Exists(pic_filepath))
-					File.Create(pic_filepath).Close();
+
+				FileStream a = new FileStream(pic_filepath, FileMode.Create);
+				a.Close();
 
 				byte[] bytes = new Byte[1024];
 				string request = null;
@@ -38,7 +39,8 @@ namespace car_rental_server
 				handler.Send(Encoding.UTF8.GetBytes("SUCCESS \r\n"));
 
 				byte[] b = new byte[length + 100];
-				BinaryWriter bw = new BinaryWriter(new FileStream(pic_filepath, FileMode.Append));
+				FileStream fs = new FileStream(pic_filepath, FileMode.Append);
+				BinaryWriter bw = new BinaryWriter(fs);
 				for (int l = 0; l < length;)
 				{
 					int len = handler.Receive(b);
@@ -46,6 +48,7 @@ namespace car_rental_server
 					l += len;
 				}
 				bw.Close();
+				fs.Close();
 
 				string sql = "INSERT INTO unsure_user(account, password, phone, pic_filepath, score, username, money) VALUES('"
 				+ args[1] + "','" + args[2] + "','" + args[4] + "','" + pic_filepath + "',0,'" + args[3] + "',0);";
@@ -77,7 +80,10 @@ namespace car_rental_server
 				}
 				rdr.Close();
 				if (!has_next)
+				{
 					handler.Send(Encoding.UTF8.GetBytes("EMPTY \r\n"));
+					return;
+				}
 
 				byte[] b = new Byte[1024];
 				string request = null;
@@ -89,7 +95,10 @@ namespace car_rental_server
 						break;
 				}
 				if (!request.Split(' ')[0].Equals("SUCCESS"))
+				{
 					handler.Send(Encoding.UTF8.GetBytes("OTHER_WRONG \r\n"));
+					return;
+				}
 
 				FileStream fileStream = new FileStream(file_path, FileMode.Open, FileAccess.Read);
 				BinaryReader binaryReader = new BinaryReader(fileStream);
@@ -97,6 +106,7 @@ namespace car_rental_server
 				byte[] bytes = new byte[length];
 				binaryReader.Read(bytes, 0, bytes.Length);
 				binaryReader.Close();
+				fileStream.Close();
 				handler.Send(Encoding.UTF8.GetBytes(length.ToString() + "\r\n"));
 
 				b = new Byte[1024];
@@ -109,7 +119,10 @@ namespace car_rental_server
 						break;
 				}
 				if (!request.Split(' ')[0].Equals("SUCCESS"))
+				{
 					handler.Send(Encoding.UTF8.GetBytes("OTHER_WRONG \r\n"));
+					return;
+				}
 
 				handler.Send(bytes);
 			}
@@ -119,6 +132,67 @@ namespace car_rental_server
 				handler.Send(Encoding.UTF8.GetBytes("OTHER_WRONG \r\n"));
 				return;
 			}
+		}
+
+		public static int register_approve(Socket handler, string account)
+		{
+			// REGISTER_APPROVE ACCOUNT \r\n
+			try
+			{
+				string[] information = new string[7];
+				string sql = "SELECT account, password, phone, pic_filepath, score, username," +
+							"money FROM unsure_user WHERE account = '" + account + "'; ";
+				MySqlCommand cmd = new MySqlCommand(sql, CarRentalServer.conn_db);
+				MySqlDataReader rdr = cmd.ExecuteReader();
+				bool has_next = false;
+				if (rdr.Read())
+				{
+					has_next = true;
+					information[0] = rdr[0].ToString();
+					information[1] = rdr[1].ToString();
+					information[2] = rdr[2].ToString();
+					information[3] = rdr[3].ToString();
+					information[4] = rdr[4].ToString();
+					information[5] = rdr[5].ToString();
+					information[6] = rdr[6].ToString();
+				}
+				rdr.Close();
+				if (!has_next)
+					return -1;
+
+				sql = "DELETE FROM unsure_user WHERE account='" + account + "';";
+				cmd = new MySqlCommand(sql, CarRentalServer.conn_db);
+				cmd.ExecuteNonQuery();
+
+				sql = "INSERT INTO user(account, password, phone, pic_filepath, score, username, money) VALUES('"
+				+ information[0] + "','" + information[1] + "','" + information[2] + "','" + information[3]
+				+ "'," + information[4] + ",'" + information[5] + "'," + information[6] + ");";
+				cmd = new MySqlCommand(sql, CarRentalServer.conn_db);
+				cmd.ExecuteNonQuery();
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.ToString());
+				return -1;
+			}
+			return 0;
+		}
+
+		public static int register_fail(Socket handler, string account)
+		{
+			// REGISTER_FAIL ACCOUNT \r\n
+			try
+			{
+				string sql = "DELETE FROM unsure_user WHERE account='" + account + "';";
+				MySqlCommand cmd = new MySqlCommand(sql, CarRentalServer.conn_db);
+				cmd.ExecuteNonQuery();
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.ToString());
+				return -1;
+			}
+			return 0;
 		}
 	}
 }
